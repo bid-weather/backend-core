b5e6f82cbc60c4259a268caebd8f8ffb3fb05769
Eugene Shin <tdw0312@gmail.com>
2026-05-15 03:10:43 +0900

    :bug: Fix: V18 마이그레이션에 category_subcategory의 대분류 업데이트 추가

--- src/main/resources/db/migration/V18__reorganize_category_and_update_announcement.sql
+++ src/main/resources/db/migration/V18__reorganize_category_and_update_announcement.sql
@@ -1,6 +1,9 @@
 UPDATE announcement SET category_id = 2 WHERE category_id IN (2, 3);
 UPDATE announcement SET category_id = 3 WHERE category_id = 4;
-DELETE FROM category WHERE id = 4;
+UPDATE category_subcategory SET category_id = 2 WHERE category_id IN (2, 3);
+UPDATE category_subcategory SET category_id = 3 WHERE category_id = 4;
 
 UPDATE category SET category_name = '용역' WHERE id = 2;
 UPDATE category SET category_name = '물품' WHERE id = 3;
+
+DELETE FROM category WHERE id = 4;
